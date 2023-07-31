43caae4f6f05922778e780e3344aba11898bb9a7
Pannisa (Eem) Lekphet <ple@manaosoftware.com>
2023-07-31 15:43:14 +0700

    Initial Commit

---
+++ WaetherForecastSwiftUI/WaetherForecastSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaetherForecastSwiftUIApp.swift
+//  WaetherForecastSwiftUI
+//
+//  Created by Pannisa Eem Lekphet on 31/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct WaetherForecastSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
